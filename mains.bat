bf3b96c016a41ff45d08ef0f04755bfd7519eaca
athdot <athdot@gmail.com>
2017-01-31 13:05:15 -0700

    Create mains.bat

---
+++ mains.bat
@@ -0,0 +1,22 @@
+:top
+IF EXIST %USERPROFILE%\Desktop\MASTERFOLDER\denied.txt ( 
+ren %USERPROFILE%\Documents\MASTERFOLDER\ denied.txt passcheck.txt 
+) 
+IF EXIST %USERPROFILE%\Documents\MASTERFOLDER\passcheck.txt ( 
+start %USERPROFILE%\Desktop\Password.bat 
+start %USERPROFILE%\Documents\MASTERFOLDER\trippymeme.jpg 
+start "" https://www.google.com/search?q=dank+memes&safe=strict&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjDgqS2zsnRAhWKqlQKHZR4CqAQ_AUICCgB&biw=1440&bih=794 
+start "" https://memegenerator.net/
+start %USERPROFILE%\Documents\MASTERFOLDER\1.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\2.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\3.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\4.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\5.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\6.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\7.jpg
+start %USERPROFILE%\Documents\MASTERFOLDER\8.png
+goto :top
+) ELSE (
+msg * I see you have cracked the code
+pause
+)
